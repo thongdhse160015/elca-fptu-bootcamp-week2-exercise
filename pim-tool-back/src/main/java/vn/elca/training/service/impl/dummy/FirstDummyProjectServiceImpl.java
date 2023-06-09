9209a89ae61574e04f247d10242f55d4ff49a326
thongdanghoang <thongdh3401@gmail.com>
2023-06-09 10:18:20 +0700

    split and add new endpoint project/search/{id}

--- pim-tool-back/src/main/java/vn/elca/training/service/impl/dummy/FirstDummyProjectServiceImpl.java
+++ pim-tool-back/src/main/java/vn/elca/training/service/impl/dummy/FirstDummyProjectServiceImpl.java
@@ -1,10 +1,12 @@
 package vn.elca.training.service.impl.dummy;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import vn.elca.training.model.entity.Project;
 import vn.elca.training.service.ProjectService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,16 +15,19 @@ import java.util.List;
  */
 @Component
 @Profile("dummy")
+@Qualifier("firstDummyProjectServiceImpl")
 public class FirstDummyProjectServiceImpl extends AbstractDummyProjectService implements ProjectService {
 
     @Override
     public List<Project> findAll() {
-        throw new UnsupportedOperationException("This is first dummy service");
+        // throw new UnsupportedOperationException("This is first dummy service");
+        return new ArrayList<>();
     }
 
     @Override
     public long count() {
-        printCurrentActiveProfiles();
-        throw new UnsupportedOperationException("This is first dummy service");
+        // printCurrentActiveProfiles();
+        // throw new UnsupportedOperationException("This is first dummy service");
+        return 0;
     }
 }
--- pim-tool-back/src/main/java/vn/elca/training/web/ProjectsController.java
+++ pim-tool-back/src/main/java/vn/elca/training/web/ProjectsController.java
@@ -4,13 +4,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.elca.training.model.dto.ProjectDto;
@@ -20,22 +17,21 @@ import vn.elca.training.service.ProjectService;
  * @author thomas.dang - thongdhse160015
  *
  */
-@Profile("dummy")
-// @Profile("!dummy | dev")
+@Profile("!dummy | dev")
 @RestController
-@RequestMapping("/project")
-public class ProjectController extends AbstractApplicationController {
+@RequestMapping("/projects")
+public class ProjectsController extends AbstractApplicationController {
 
     @Autowired
     private ProjectService projectService;
 
-    @GetMapping("/search/{id}")
-    @ResponseBody
-    public ProjectDto getProjectById(@PathVariable long id) {
+    @GetMapping("/search")
+    public List<ProjectDto> search(@RequestParam String keyword) {
         return projectService.findAll()
                 .stream()
-                .filter(product -> product.getId() == id)
+                .filter(product -> product.getName().trim().toLowerCase()
+                        .contains(keyword.trim().toLowerCase()))
                 .map(mapper::projectToProjectDto)
-                .findFirst().orElseThrow();
+                .collect(Collectors.toList());
     }
 }
