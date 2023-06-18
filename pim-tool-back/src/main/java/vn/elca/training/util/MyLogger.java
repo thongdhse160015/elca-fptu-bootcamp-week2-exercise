51d07b44ef080f25f0308bb4c659d63dd82096f7
thomas.dang <thongdh3401@gmail.com>
2023-06-18 12:25:00 +0700

    config log inside log console

--- pim-tool-back/src/main/java/vn/elca/training/util/MyLogger.java
+++ pim-tool-back/src/main/java/vn/elca/training/util/MyLogger.java
@@ -1,28 +1,23 @@
 package vn.elca.training.util;
 
-import java.io.IOException;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 public class MyLogger {
-    private static MyLogger instance;
-    private Logger logger;
-
     private static final String FILE_NAME = "application.log";
+    private static MyLogger instance;
+    private final Logger logger;
 
     private MyLogger() {
         logger = Logger.getLogger(MyLogger.class.getName());
-
-        try {
-            // Configure the FileHandler to write logs to a file named "application.log"
-            FileHandler fileHandler = new FileHandler(FILE_NAME);
-            fileHandler.setFormatter(new SimpleFormatter());
-            logger.addHandler(fileHandler);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            // Configure the FileHandler to write logs to a file named "application.log"
+//            FileHandler fileHandler = new FileHandler(FILE_NAME);
+//            fileHandler.setFormatter(new SimpleFormatter());
+//            logger.addHandler(fileHandler);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public static MyLogger getInstance() {
