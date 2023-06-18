3a9db764fb729771370dbad76695f2debc5c02d0
thomas.dang <thongdh3401@gmail.com>
2023-06-18 12:22:36 +0700

    write createMaintenanceProject implementation

--- pim-tool-back/src/main/java/vn/elca/training/service/impl/ProjectServiceImpl.java
+++ pim-tool-back/src/main/java/vn/elca/training/service/impl/ProjectServiceImpl.java
@@ -1,22 +1,21 @@
 package vn.elca.training.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import vn.elca.training.model.entity.Project;
 import vn.elca.training.repository.ProjectRepository;
 import vn.elca.training.service.ProjectService;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
  * @author vlp
- *
  */
 @Service
 @Profile("!dummy | dev")
-@Qualifier("projectServiceImpl")
 public class ProjectServiceImpl implements ProjectService {
 
     @Autowired
@@ -40,5 +39,31 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     public Project updateProject(Project project) {
         return projectRepository.save(project);
+    }
+
+    @Override
+    @Transactional
+    public void createMaintenanceProject(long oldProjectId) {
+        // Retrieve the old project from the repository
+        Project oldProject = projectRepository.findById(oldProjectId)
+                .orElseThrow(RuntimeException::new);
+
+        // Construct the name of the new project
+        String newProjectName = oldProject.getName() + " Maint. " + LocalDate.now().getYear();
+
+        // Create the maintenance project
+        Project maintenanceProject = new Project();
+        maintenanceProject.setName(newProjectName);
+        maintenanceProject.setActivated(false);
+
+        // Save the maintenance project
+        projectRepository.save(maintenanceProject);
+
+        // Update the old project to be inactive
+        oldProject.setActivated(false);
+        projectRepository.save(oldProject);
+
+        // Simulate an exception to test rollback behavior (optional)
+        // throw new RuntimeException("Simulated exception");
     }
 }
