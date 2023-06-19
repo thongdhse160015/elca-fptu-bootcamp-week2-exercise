3a71c5fc51bdc81232ebca18d7c2b986e2c8c969
thomas.dang <thongdh3401@gmail.com>
2023-06-19 21:59:37 +0700

    Recursive entity in request response

--- pim-tool-back/src/main/java/vn/elca/training/model/entity/Task.java
+++ pim-tool-back/src/main/java/vn/elca/training/model/entity/Task.java
@@ -13,12 +13,17 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * @author vlp
- *
  */
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Task implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,53 +42,12 @@ public class Task implements Serializable {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn
+    @JsonIgnore
     private User user;
 
     public Task(Project project, String name) {
         this.project = project;
         this.name = name;
         this.deadline = LocalDate.now();
-    }
-
-    public Task() {}
-
-    public Project getProject() {
-        return project;
-    }
-
-    public void setProject(Project project) {
-        this.project = project;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(LocalDate deadline) {
-        this.deadline = deadline;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
     }
 }
