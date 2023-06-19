fbc554f3327b9e0118b68c630722318dbce0bd02
thomas.dang <thongdh3401@gmail.com>
2023-06-19 21:59:20 +0700

    Make tasks added for user

--- pim-tool-back/src/main/resources/data.sql
+++ pim-tool-back/src/main/resources/data.sql
@@ -1,2 +1,13 @@
 INSERT INTO User (username, role) VALUES
   ('thomas.dang', 'DEV');
+INSERT INTO task (name, deadline)
+VALUES ('Task 1', '2023-06-01'),
+       ('Task 2', '2023-06-02'),
+       ('Task 3', '2023-06-03'),
+       ('Task 4', '2023-06-04'),
+       ('Task 5', '2023-06-05'),
+       ('Task 6', '2023-06-06'),
+       ('Task 7', '2023-06-07'),
+       ('Task 8', '2023-06-08'),
+       ('Task 9', '2023-06-09'),
+       ('Task 10', '2023-06-10');
